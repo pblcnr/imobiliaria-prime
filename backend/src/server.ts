0f23cfa5640041e657bbe53b03cc504a291f9b41
Paulo Henrique de Andrade <paulohenrique.andradefilho@gmail.com>
2025-12-03 14:56:28 -0300

    feat: integrate employee routes into express server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,20 @@
 import express from "express";
 import { errorHandler } from "./shared/middlewares/index.js";
 import { env } from "./config/env.js";
+import employeeRoutes from "./modules/employees/routes.js";
 
 const app = express();
 
 app.use(express.json());
 
+// Routes
 app.get("/health", (req, res) => {
   res.send({ status: "OK" });
 });
 
+app.use("/employees", employeeRoutes);
+
+// Error Handler
 app.use(errorHandler);
 
 app.listen(env.PORT, () => {
